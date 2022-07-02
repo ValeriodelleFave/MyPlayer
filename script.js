114a15769b633b9d73411e27874d670d76368db3
ValeriodelleFave <valerio.dellefave00@gmail.com>
2022-07-02 20:17:10 +0200

    +

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-async function init() {
-    let d = await fetchAsync("http://localhost:3000/player");
-    console.log(typeof d);
+function init() {
+
 }
 
 async function fetchAsync(url) {
@@ -8,3 +7,22 @@ async function fetchAsync(url) {
     let data = await response.json();
     return data;
 }
+
+function loadFile(params) {
+    console.log(params.target)
+}
+
+
+document.getElementById("inputVideo").addEventListener('change', function() {
+    const file = this.files[0];
+    const video = document.getElementById("video");
+
+    
+    //video.attributes.getNamedItem('src').value = file;
+     
+    debugger
+    var source = document.getElementById('source');   
+    source.setAttribute('sizes', file.size);
+    source.setAttribute('type', file.type);
+   video.play()
+})
